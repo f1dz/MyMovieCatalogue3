5fd9c3e9753971f469be7d11f3f4202130d79615
Rian <rian.sj@gmail.com>
2019-10-07 17:32:08 +0700

    initial commit

---
+++ app/src/main/java/com/lonecode/mymoviecatalogue3/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.lonecode.mymoviecatalogue3.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Tv Show fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/lonecode/mymoviecatalogue3/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.lonecode.mymoviecatalogue3.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is movie fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
