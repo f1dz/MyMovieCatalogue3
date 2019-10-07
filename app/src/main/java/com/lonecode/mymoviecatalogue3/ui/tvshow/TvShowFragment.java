c19996239c1046a5cdda6d260544704590816188
Rian <rian.sj@gmail.com>
2019-10-07 18:04:33 +0700

    done refactoring

--- app/src/main/java/com/lonecode/mymoviecatalogue3/ui/tvshow/TvShowFragment.java
+++ app/src/main/java/com/lonecode/mymoviecatalogue3/ui/tvshow/TvShowFragment.java
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.lonecode.mymoviecatalogue3.R;
 
-public class DashboardFragment extends Fragment {
+public class TvShowFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private TvShowViewModel tvshowViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
+        tvshowViewModel =
+                ViewModelProviders.of(this).get(TvShowViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_tvshow, container, false);
+        final TextView textView = root.findViewById(R.id.text_tvshow);
+        tvshowViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
